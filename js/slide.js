c477be6ac2d3318adaa86d56c090fbf70cf7bec6
vidit gupta <xmas@vidit-guptas-MacBook-Pro-7.local>
2016-06-30 22:13:02 +0530

    completed  ss

--- js/slide.js
+++ js/slide.js
@@ -6,5 +6,21 @@ function slide(dir) {
   } else {
     left=left+100;
   }
+  updateArrow();
   document.querySelector('.slide-container').style.left=left+"%";
+}
+
+function updateArrow() {
+  if (left==0) {
+    document.querySelector(".left").classList.add("hide");
+  } else if (left==-400) {
+    document.querySelector(".right").classList.add("hide");
+  } else {
+    document.querySelector(".left").classList.remove("hide");
+    document.querySelector(".right").classList.remove("hide");
+  }
+}
+
+window.onload=function () {
+  updateArrow();
 }
